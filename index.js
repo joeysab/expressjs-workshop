f81ad77bab71a7734f9e2b9ae5774c2091a163e4
joeysab <josephsabbagh@live.com>
2016-05-02 20:20:42 +0000

    Question 3 Done

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function (req, res) {
-  res.send('Hello World!');
+
+app.get("/hello", function(req, res) {
+  if (req.query.name){
+    res.end("<h1>Hello, " + req.query.name + ".");
+  }else {
+    res.send('Hello World!');
+  }
 });
 
 
 
-
+app.get('/calculator/:operation/:num1/:num2', function(req, res) {
+  
+  console.log(req.params)
+  
+    var num1 = Number(req.params.num1);
+    var num2 = Number(req.params.num2);
+    var operator = req.params.operation;
+    
+    var result;
+    if (operator === "add"){
+     result = num1+num2;
+    } else if (operator === "sub"){
+     result = num1-num2;
+    }else if (operator === "mult"){
+     result = num1*num2;
+    }else if (operator === "div"){
+      result = num1/num2
+    }
+    res.send({
+      operator: operator,
+      firstOperand: num1,
+      secondOperand: num2,
+      solution: result
+    })
+  
+});
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
